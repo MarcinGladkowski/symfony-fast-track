4a7ed6d0d8775bfdd63ca0d548e3d753c6544976
MarcinGladkowski <marcingladkowski@gmail.com>
2022-04-27 16:40:15 +0200

    feat: working on tests - finising at https://symfony.com/doc/current/the-fast-track/en/17-tests.html

--- tests/ConferenceControllerTest.php
+++ tests/ConferenceControllerTest.php
@@ -39,10 +39,9 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertCount(2, $crawler->filter('h4'));
 
         $client->clickLink('View');
-
         self::assertPageTitleContains('Amsterdam');
         self::assertResponseIsSuccessful();
         self::assertSelectorTextContains('h2', 'Amsterdam 2019');
-        self::assertSelectorExists('div:contains("There are 1 comments")');
+        self::assertSelectorExists('div:contains("There are 2 comments")');
     }
 }
