126bd45295333446b260f3064c0cc90a12086122
MarcinGladkowski <marcingladkowski@gmail.com>
2022-04-27 15:48:33 +0200

    feat: add admin fixture with password hasher

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,25 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Admin;
 use App\Entity\Comment;
 use App\Entity\Conference;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private PasswordHasherFactoryInterface $encoderFactory) {}
+
     public function load(ObjectManager $manager): void
     {
+        $admin = new Admin();
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setUsername('admin');
+        $admin->setPassword($this->encoderFactory->getPasswordHasher(Admin::class)->hash('admin', null));
+        $manager->persist($admin);
+
         $amsterdam = new Conference();
         $amsterdam->setCity('Amsterdam');
         $amsterdam->setYear('2019');
