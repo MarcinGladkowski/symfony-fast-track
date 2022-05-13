ddf064f57b41f1036e82ccb090ab517bbccd8c0d
MarcinGladkowski <marcingladkowski@gmail.com>
2022-05-13 08:05:43 +0200

    feat: plural translations
    https://symfony.com/doc/current/the-fast-track/en/28-intl.html

--- tests/ConferenceControllerTest.php
+++ tests/ConferenceControllerTest.php
@@ -16,7 +16,7 @@ class ConferenceControllerTest extends WebTestCase
 
         $client = static::createClient();
 
-        $client->request('GET', '/');
+        $client->request('GET', '/en');
 
         self::assertResponseIsSuccessful();
         self::assertSelectorTextContains('h2', 'Give your feedback');
@@ -25,7 +25,7 @@ class ConferenceControllerTest extends WebTestCase
     public function testCommentSubmission(): void
     {
         $client = static::createClient();
-        $client->request('GET', '/conference/amsterdam-2019');
+        $client->request('GET', '/en/conference/amsterdam-2019');
         $client->submitForm('Submit', [
             'comment_form[author]' => 'Fabien',
             'comment_form[text]' => 'Some feedback from an automated functional test',
@@ -44,7 +44,7 @@ class ConferenceControllerTest extends WebTestCase
     public function testConferencePage()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/en/');
 
         $this->assertCount(2, $crawler->filter('h4'));
 
